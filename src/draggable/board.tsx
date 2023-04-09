5f4c7374ee77f2e68d71159be2ba2cc0b9fe7f13
devstefancho <skku7714@gmail.com>
2023-04-10 00:06:03 +0900

    Style: 보드 기본스타일링 완료

--- src/draggable/board.tsx
+++ src/draggable/board.tsx
@@ -8,27 +8,46 @@ const Board = ({ boardKey }: { boardKey: keyof DragItems }) => {
   const { dragItemsInBoards } = useDraggableList();
 
   return (
-    <Droppable droppableId={String(boardKey)}>
-      {(provided) => (
-        <StyledContainer {...provided.droppableProps} ref={provided.innerRef}>
-          {dragItemsInBoards[boardKey].map((item, index) => (
-            <DraggableCard item={item} index={index} />
-          ))}
-          {provided.placeholder}
-        </StyledContainer>
-      )}
-    </Droppable>
+    <BoardContainer>
+      <h1>{boardKey}</h1>
+      <Droppable droppableId={String(boardKey)}>
+        {(provided) => (
+          <CardContainer {...provided.droppableProps} ref={provided.innerRef}>
+            {dragItemsInBoards[boardKey].map((item, index) => (
+              <DraggableCard item={item} index={index} />
+            ))}
+            {provided.placeholder}
+          </CardContainer>
+        )}
+      </Droppable>
+    </BoardContainer>
   );
 };
 
-const StyledContainer = styled.div`
+const BoardContainer = styled.div`
+  background-color: #ccc;
+  padding: 10px;
+  border-radius: 5px;
+
+  h1 {
+    background-color: #ccc;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 700;
+    color: #fff;
+    padding: 10px;
+  }
+`;
+
+const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   background-color: #fff;
   padding: 20px;
-  border-radius: 5px;
+  min-height: 300px;
+  min-width: 250px;
 `;
 
 export default Board;
